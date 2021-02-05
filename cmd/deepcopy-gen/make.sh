856dc451e9c9fa23f5714fa2510e6df2a7b481ed
nevercase <1024769485@qq.com>
2021-02-05 11:55:04 +0800

    make.sh

--- cmd/deepcopy-gen/make.sh
+++ cmd/deepcopy-gen/make.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export GOPATH=`go env | grep -i gopath | awk '{split($0,a,"\""); print a[2]}'`
+
 go build -o deepcopy-gen main.go
-cp deepcopy-gen /Users/nevermore/go/bin/
+cp deepcopy-gen ${GOPATH}/bin/
 rm -f deepcopy-gen
--- cmd/go-to-protobuf/make.sh
+++ cmd/go-to-protobuf/make.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+export GOPATH=`go env | grep -i gopath | awk '{split($0,a,"\""); print a[2]}'`
+
 go build -o go-to-protobuf main.go
-cp go-to-protobuf /Users/nevermore/go/bin/
+cp go-to-protobuf ${GOPATH}/bin/
 rm -f go-to-protobuf
 
-#go build -o protoc-gen-gogo-bin protoc-gen-gogo/main.go
-#cp protoc-gen-gogo-bin /Users/nevermore/go/bin/protoc-gen-gogo
-#rm -f protoc-gen-gogo-bin
+go build -o protoc-gen-gogo-bin protoc-gen-gogo/main.go
+cp protoc-gen-gogo-bin ${GOPATH}/bin/protoc-gen-gogo
+rm -f protoc-gen-gogo-bin
